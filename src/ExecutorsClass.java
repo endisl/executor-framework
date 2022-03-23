81eea81dd42741141e6e21c82a8adfedd60518ac
endisl <endiluamba@gmail.com>
2022-03-23 20:51:32 -0300

    callables and futures

--- src/ExecutorsClass.java
+++ src/ExecutorsClass.java
@@ -1,5 +1,7 @@
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ExecutorsClass {
 
@@ -7,7 +9,18 @@ public class ExecutorsClass {
         var executor = Executors.newFixedThreadPool(2);
 
         try {
-            executor.submit(() -> System.out.println(Thread.currentThread().getName()));
+            var future = executor.submit(() -> {
+                LongTask.simulate();
+                return 1;
+            });
+
+            System.out.println("Do more work");
+            try {
+                var result = future.get();
+                System.out.println(result);
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
         }
         finally {
             executor.shutdown();
---
+++ src/LongTask.java
@@ -0,0 +1,9 @@
+public class LongTask {
+    public static void simulate() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
