90a7abe71db189d12170e5f2e146f327fed8137a
endisl <endiluamba@gmail.com>
2022-03-24 16:11:18 -0300

    handling exceptions

--- src/CompletableFuturesClass.java
+++ src/CompletableFuturesClass.java
@@ -5,17 +5,31 @@ import java.util.function.Supplier;
 
 public class CompletableFuturesClass {
     public static void show() {
-        var future = CompletableFuture.supplyAsync(() -> 1);
-        //future.thenRun(() -> System.out.println("Done"));
-        /*future.thenRunAsync(() -> {
+        var future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Getting the current weather");
+            throw new IllegalStateException();
+        });
+
+        try {
+            var temperature = future.exceptionally(ex -> 1).get();
+            System.out.println(temperature);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+        /*var future = CompletableFuture.supplyAsync(() -> 1);
+        future.thenRun(() -> System.out.println("Done"));
+        future.thenRunAsync(() -> {
             System.out.println(Thread.currentThread().getName());
             System.out.println("Done");
-        });*/
+        });
         future.thenAccept(result -> {
             System.out.println(Thread.currentThread().getName());
             System.out.println(result);
-        });
-
+        });*/
 
         /*Runnable task1 = () -> System.out.println("a");
         var future1 = CompletableFuture.runAsync(task1);
