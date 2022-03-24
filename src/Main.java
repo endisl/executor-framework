df0afd49fad338f34aedc6692dfa5a911bade4e8
endisl <endiluamba@gmail.com>
2022-03-24 14:57:25 -0300

    implementing an asynchronous api

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
+        var service = new MailService();
+        service.sendAsync();
+        System.out.println("Hello Ndion");
+
+
+
 
         //ExecutorsClass.show();
-        CompletableFuturesClass.show();
+        //CompletableFuturesClass.show();
     }
 }
